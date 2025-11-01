a90fb9a2737f516b6c8a0280d794c531030058b0
Moisei-D <daniel.moisei@isa.utm.md>
2025-11-01 15:30:17 +0200

    finished cuba task

--- CUBA/main.py
+++ CUBA/main.py
@@ -1,11 +1,27 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+
 with open('CUBA/input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+    sentences = f.readlines()
 
-# Remove periods at the end of sentences
-text = text.replace('.', '')
+stop_words = set(stopwords.words('english'))
+filtered_sentences = []
 
-# Write to output.txt
+for sentence in sentences:
+    if sentence:
+        word_tokens = word_tokenize(sentence)
+        filtered_words = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = " ".join(filtered_words)
+        #some space replaces
+        filtered_sentence = filtered_sentence.replace(" .", ".").replace(" ,",",").replace(" '", "'")
+        filtered_sentences.append(filtered_sentence)
+        
+
 with open('CUBA/output.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
+    for sentence in filtered_sentences:
+        f.write(sentence + '\n')
 
-print("Processed text saved to output.txt")
