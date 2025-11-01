3794755d2f1db490b23aa525eb86b7a35f390774
Canter Daniel <83081029+The1Dani@users.noreply.github.com>
2025-11-02 01:31:30 +0200

    canada solution

---
+++ canada/main.py
@@ -0,0 +1,41 @@
+import csv
+
+def mySplit(arr):
+    translated_text = arr.translate(str.maketrans({'\'': ' \' ', '.': ' . ','?': ' ? '}))
+    return translated_text.split()
+
+d = {}
+
+with open('dinosaur_dataset.csv', 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        first = False
+        for row in csv_reader:
+            if not first:
+                first = True
+                continue
+            
+            for k,v in zip(mySplit(row[0]), mySplit(row[1])):
+                 d[k] = v
+# print(d)
+
+sentences = ['sentence']
+with open('test-input.csv', 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    first = False
+    for row in csv_reader:
+        f_word = ""
+        sentence = []
+        if not first:
+            first = True
+            continue
+        for k in mySplit(row[0]):
+            sentence.append(d[k]) 
+        # f_word = " ".join(word[:-1])
+        f_word = " ".join(sentence)
+        # f_word = "".join([f_word, word[-1]])
+        sentences.append(f_word)
+        # print(f_word)
+fileContent = "\n".join(sentences)
+print(fileContent)
+with open("output.csv", "w") as f:
+     f.write(fileContent)
